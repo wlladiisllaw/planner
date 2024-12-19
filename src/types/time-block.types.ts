914ff4c9f470feaa6ea0e33fdf80100e9c434053
vvvladislovvve <vslav13613@gmail.com>
2024-12-19 16:12:56 +0500

    init project

---
+++ src/types/time-block.types.ts
@@ -0,0 +1,10 @@
+import { IBase } from "./root.types"
+
+export interface ITimeBlockResponse extends IBase {
+    name: string
+    color?: string
+    duration: number
+    order: number
+}
+
+export type TypeTimeBlockFormState = Partial<Omit<ITimeBlockResponse, 'createdAt' | 'updateAt'>>
